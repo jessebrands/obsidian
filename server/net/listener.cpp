e4fdfe182bcd8fed30c347cb83221569d7855c7f
Jesse Gerard Brands <jesse@jgbrands.nl>
2024-10-26 23:47:26 +0200

    net/listener: added io::executor dependency

--- server/net/listener.cpp
+++ server/net/listener.cpp
@@ -25,8 +25,9 @@
 constexpr static int enable = 1;
 
 namespace obsidian::net {
-    listener::listener(addrinfo const* address_info, int const backlog)
-        : socket_(address_info->ai_family, address_info->ai_socktype, address_info->ai_protocol),
+    listener::listener(io::executor& executor, addrinfo const* address_info, int const backlog)
+        : executor_(executor),
+          socket_(address_info->ai_family, address_info->ai_socktype, address_info->ai_protocol),
           address_(address_info->ai_addr, address_info->ai_addrlen) {
         if (setsockopt(socket_.handle(), SOL_SOCKET, SO_REUSEADDR, &enable, sizeof enable) < 0) {
             throw std::system_error{errno, std::generic_category(), "setsockopt"};
@@ -41,7 +42,8 @@ namespace obsidian::net {
         }
     }
 
-    listener listener::create(std::string const& address, std::string const& port, int const backlog) {
+    listener listener::create(io::executor& executor,
+                              std::string const& address, std::string const& port, int const backlog) {
         addrinfo info = {};
         info.ai_family = AF_UNSPEC;
         info.ai_socktype = SOCK_STREAM;
@@ -57,7 +59,7 @@ namespace obsidian::net {
 
         for (addrinfo* p = serv_info.get(); p != nullptr; p = p->ai_next) {
             try {
-                return listener{p, backlog};
+                return listener{executor, p, backlog};
             }
             catch (...) {
                 // TODO: Potentially log a warning.
--- server/net/listener.hpp
+++ server/net/listener.hpp
@@ -24,20 +24,26 @@
 #include "net/address.hpp"
 #include "net/socket.hpp"
 
+namespace obsidian::io {
+    class executor;
+}
+
 namespace obsidian::net {
     /*!
      * \brief Implements a TCP/IPv6 listening socket.
      */
     class listener {
+        io::executor& executor_;
         socket socket_;
         address address_;
 
         /*!
          * Initializes a listener.
+         * \param executor I/O executor to use for this listener.
          * \param address_info Info structure detailing what kind of socket and listener this is.
          * \param backlog Amount of queued up connections in the accept queue.
          */
-        explicit listener(addrinfo const* address_info, int backlog = default_backlog);
+        explicit listener(io::executor& executor, addrinfo const* address_info, int backlog = default_backlog);
 
     public:
         /// Default amount of connections to keep in the kernel accept queue.
@@ -45,6 +51,7 @@ namespace obsidian::net {
 
         /*!
          * Constructs a TCP listener.
+         * \param executor I/O executor to use for the created listener.
          * \param address Address to bind the listener to.
          * \param port Port to bind the listener to.
          * \param backlog Amount of queued up connections to keep in the accept queue.
@@ -52,7 +59,8 @@ namespace obsidian::net {
          * \note This may create either a IPv4 or IPv6 listener, depending on the value of address. This factory
          *       function makes use of getaddrinfo() to resolve what kind of listener socket to create.
          */
-        static listener create(std::string const& address, std::string const& port, int backlog = default_backlog);
+        static listener create(io::executor& executor,
+                               std::string const& address, std::string const& port, int backlog = default_backlog);
 
         /*!
          * Gets the address of this listener.
